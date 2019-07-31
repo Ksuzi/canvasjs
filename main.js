6606fb4e7afcf5162591f5757f6c50be40fa924e
Ksenia <53110264+Ksuzi@users.noreply.github.com>
2019-07-31 19:01:17 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
+const canvasDiv = document.querySelector('.display');
+const buttons = document.querySelectorAll('[data-action]');
 
-const backgroundDiv = document.querySelector('.canvas');
+const lineSize = document.querySelector('#size');
+const lineSizeDisplay = document.querySelector('.lineSizeDisplay');
 
-const lineSizeDisplay = document.querySelector('#line-size');
-const lineSizeRange = document.querySelector('#size');
-const palleteBtn = document.querySelector('#pallete');
-const eraserBtn = document.querySelector('#eraser');
-const pencilBtn = document.querySelector('#pencil');
-const clearAllBtn = document.querySelector('#clear');
-const backgroundColorBtn = document.querySelector('#background');
+const eraserCursor =  'url("https://c.radikal.ru/c10/1907/88/5ab7757bf718.png"), auto';
+const pencilCursor = 'url("https://a.radikal.ru/a13/1907/86/113d58217694.png"), auto';
 
 const changeColor = document.querySelector('#color');
 const changeBackgroundColor = document.querySelector('#backgroundColor');
 
+const palleteBtn = document.querySelector(".lineColor");
 palleteBtn.style.color = changeColor.value;
-lineSizeDisplay.innerHTML = lineSizeRange.value + 'px';
 
-canvas.width = window.innerWidth * 0.895;
-canvas.height = window.innerHeight * 0.743;
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight * 0.898;
 ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
 
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
@@ -32,23 +29,11 @@ let lastY = 0;
 let isErase = false;
 
 function resize() {
-  ctx.canvas.width = window.innerWidth * 0.895;
-  ctx.canvas.height = window.innerHeight * 0.743;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight * 0.898;
   ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
-  //backgroundDiv.style.backgroundColor = "#fff";
+  canvasDiv.style.backgroundColor = "#fff";
   changeBackgroundColor.value = "#ffffff";
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-}
-
-function clearCanvas(){
-  ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
-  backgroundDiv.style.backgroundColor = "#fff";
-  changeBackgroundColor.value = "#ffffff";
-  isErase = false;
-  canvas.style.cursor = 'url("https://a.radikal.ru/a13/1907/86/113d58217694.png"), auto';
-  lineSizeRange.value = '10';
-  lineSizeDisplay.innerHTML = lineSizeRange.value + 'px';
   ctx.lineJoin = 'round';
   ctx.lineCap = 'round';
 }
@@ -56,7 +41,7 @@ function clearCanvas(){
 function draw(e) {
   if (!isDrawing) return; 
   console.log(e);
-  ctx.lineWidth = lineSizeRange.value;
+  ctx.lineWidth = lineSize.value;
   
   if(isErase){
     ctx.clearRect(e.offsetX, e.offsetY, ctx.lineWidth, ctx.lineWidth);
@@ -71,11 +56,23 @@ function draw(e) {
   }
 }
 
-function lineSizeChanging(){
-  lineSizeDisplay.innerHTML = lineSizeRange.value + 'px';
+function clear(){
+  ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
+  canvasDiv.style.backgroundColor = "#fff";
+  changeBackgroundColor.value = "#ffffff";
+  isErase = false;
+  canvas.style.cursor = pencilCursor;
+  lineSize.value = '10';
+  lineSizeDisplay.innerHTML = lineSize.value;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
 }
 
-function palleteBtnColorChanging(){
+function lineSizeChanging(){
+  lineSizeDisplay.innerHTML = lineSize.value;
+}
+
+function color(){
   changeColor.click();
 }
 
@@ -83,22 +80,22 @@ function lineColorChanging() {
   palleteBtn.style.color = changeColor.value;
 }
 
-function eraseTheLine() {
+function eraser() {
   isErase = true;
-  canvas.style.cursor = 'url("https://c.radikal.ru/c10/1907/88/5ab7757bf718.png"), auto';
+  canvas.style.cursor = eraserCursor;
 }
 
-function usePencil() {
+function pencil() {
   isErase = false;
-  canvas.style.cursor = 'url("https://a.radikal.ru/a13/1907/86/113d58217694.png"), auto';
+  canvas.style.cursor = pencilCursor;
 }
 
-function backgroundColorChanging() {
+function backgroundColor() {
   changeBackgroundColor.click();
 }
  
-function backgroundBtnChanging(){
-  backgroundDiv.style.backgroundColor = changeBackgroundColor.value;
+function backgroundBtnChanging(e){
+  canvasDiv.style.backgroundColor = changeBackgroundColor.value;
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -111,17 +108,17 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-lineSizeRange.addEventListener('change',lineSizeChanging);
-lineSizeRange.addEventListener('mousemove',lineSizeChanging);
-palleteBtn.addEventListener('click', palleteBtnColorChanging);
-changeColor.addEventListener('change',lineColorChanging);
-eraserBtn.addEventListener('click', eraseTheLine);
-pencilBtn.addEventListener('click', usePencil);
-backgroundColorBtn.addEventListener('click', backgroundColorChanging)
-changeBackgroundColor.addEventListener('change',backgroundBtnChanging);
-clearAllBtn.addEventListener('click', clearCanvas);
+lineSize.addEventListener('change',lineSizeChanging);
+lineSize.addEventListener('mousemove',lineSizeChanging);
 
-window.addEventListener("resize", resize);
+changeColor.addEventListener('change',lineColorChanging);
+changeBackgroundColor.addEventListener('change',backgroundBtnChanging);
+
+buttons.forEach(button => button.addEventListener('click', function() {
+  eval(this.dataset.action);
+}));
+
+ window.addEventListener("resize", resize);
 
 
 
